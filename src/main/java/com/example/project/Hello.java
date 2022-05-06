4928750368671a7d84e0c6a0a2b5f98c76108e60
Arelis <mchuraz@unsa.edu.pe>
2022-05-06 14:58:20 -0500

    le quite de comentarios

--- src/main/java/com/example/project/Hello.java
+++ src/main/java/com/example/project/Hello.java
@@ -6,7 +6,7 @@ import javax.sound.sampled.SourceDataLine;
 public class Hello {
 
 	public static void main(final String[] args) {
-		//System.out.println("Not the right string, tests will fail!");
+		System.out.println("Not the right string, tests will fail!");
 		System.out.println("¡Hello World!");
 	}
 
