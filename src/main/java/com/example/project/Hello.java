1a6d58b1cd2d74220f70d1d3703c081271e1f7da
Arelis <mchuraz@unsa.edu.pe>
2022-05-06 14:49:12 -0500

    hice hello world

--- src/main/java/com/example/project/Hello.java
+++ src/main/java/com/example/project/Hello.java
@@ -1,9 +1,13 @@
 package com.example.project;
 
+import javax.sound.midi.Soundbank;
+import javax.sound.sampled.SourceDataLine;
+
 public class Hello {
 
 	public static void main(final String[] args) {
 		System.out.println("Not the right string, tests will fail!");
+		System.out.println("Hello World!");
 	}
 
 }
